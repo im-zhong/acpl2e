56a83a4b102487795deec8807606896d97971611
zhangzhong <im.zhong@outlook.com>
2021-06-16 10:19:07 +0800

    C3-3.6-E6: itoa with width

--- control_flow/itoa/itoa.c
+++ control_flow/itoa/itoa.c
@@ -3,13 +3,16 @@
 // Exercise 3-4. In a two's complement number representation, our version of itoa dose not 
 //               handle the largest negative number. Explain why not. Modify it to print
 //               that value correctly.
+// Exercise 3-6. Write a version of itoa that accepts three arguments instead of two.
+//               The third argument is a minimum field width; the converted number must 
+//               be padded with blanks on the left if necessary to make it wide enough.
 
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
 
 // itoa: convert n to characters in s
-void itoa(int n, char s[])
+void itoa(int n, char s[], int width)
 {
     // negative mod is machine-relative
     // so we should convert it to positive
@@ -30,8 +33,12 @@ void itoa(int n, char s[])
     } while ((ll /= 10) > 0);
     if (sign < 0)
         s[i++] = '-';
-    s[i] = '\0';
 
+    // padded with blanks
+    while (i < width)
+        s[i++] = ' ';
+
+    s[i] = '\0';
     char c = '\0';
     --i;
     for (int j = 0; j < i; --i, ++j)
@@ -40,15 +47,16 @@ void itoa(int n, char s[])
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        printf("Usage: %s <integer>\n", argv[0]);
+        printf("Usage: %s <integer> <width>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     int n = atoi(argv[1]);
+    int w = atoi(argv[2]);
     char s[4096] = {'\0'};
-    itoa(n, s);
+    itoa(n, s, w);
     printf("%s\n", s);
     exit(EXIT_SUCCESS);
 }
